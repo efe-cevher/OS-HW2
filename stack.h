6939a817bd65203136b74acb25a967eddb92b2c7
Efe <efecan.cevher@gmail.com>
2019-10-24 18:11:12 +0300

    initial

---
+++ stack.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct stack {
+int top;
+int capacity;
+int* task;
+int* values;
+};
+
+typedef struct stack Stack;
+
+Stack* createStack(int capacity);
+int isFull(Stack* stack);
+int isEmpty(Stack* stack);
+void push(Stack* stack, int t, int item); //t refers task, item refers the value
+int pop(Stack* stack);
